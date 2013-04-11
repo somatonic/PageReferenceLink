46e7bf483927081868f3d04a63f4842a5aa875ee
somatonic <philipp@urlich.ch>
2013-04-12 01:36:33 +0200

    delegated id so an newly added item can also be edited without saving page

--- PageReferenceLink.js
+++ PageReferenceLink.js
@@ -2,6 +2,8 @@ $(function(){
     var h = $(window).height()-65;
     var w = $(window).width() > 1150 ? 1150 : $(window).width()-100;
     $('.InputfieldPageListSelectMultiple').on("click",".itemEdit",function(e){
+        if($(this).attr('href'))var id = $(this).parent().find('.itemValue').text();
+        $(this).attr("href",$(this).attr('href') + "&id=" +id);
         e.preventDefault();
         $(this).fancybox({
             hideOnContentClick: false,
