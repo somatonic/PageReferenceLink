51dfd127c49659b227bb0de4ecd43df9ae412faa
somatonic <philipp@urlich.ch>
2013-04-12 02:42:41 +0200

    added hook for asm select

--- PageReferenceLink.js
+++ PageReferenceLink.js
@@ -1,15 +1,32 @@
 $(function(){
-    var h = $(window).height()-65;
-    var w = $(window).width() > 1150 ? 1150 : $(window).width()-100;
+
     $('.InputfieldPageListSelectMultiple').on("click",".itemEdit",function(e){
-        if($(this).attr('href'))var id = $(this).parent().find('.itemValue').text();
+        var id = $(this).parent().find('.itemValue').text();
         $(this).attr("href",$(this).attr('href') + "&id=" +id);
+        var h = $(window).height()-65;
+        var w = $(window).width() > 1150 ? 1150 : $(window).width()-100;
         e.preventDefault();
         $(this).fancybox({
             hideOnContentClick: false,
-            centerOnScroll: false,
+            centerOnScroll: true,
             frameWidth: w,
             frameHeight: h
         }).trigger('click');
     });
+
+    $('.asmList').on("click",".asmItemEditModal",function(e){
+        var id = $(this).parent('li').attr('rel');
+        var pid = $(this).closest('.asmContainer').find('.asmSelect').find('option[rel="' + id + '"]').val();
+        $(this).attr("href", $(this).attr('href') + "&id=" + pid);
+        var h = $(window).height()-65;
+        var w = $(window).width() > 1150 ? 1150 : $(window).width()-100;
+        e.preventDefault();
+        $(this).fancybox({
+            hideOnContentClick: false,
+            centerOnScroll: true,
+            frameWidth: w,
+            frameHeight: h
+        }).trigger('click');
+    });
+
 });
